6d0e6e2ad7cc77ba50770ad90bc7cd911d6ac1ad
serig85 <115493415+serig85@users.noreply.github.com>
2024-12-17 14:32:33 +0300

    commit start proba

--- myapp.py
+++ myapp.py
@@ -9,21 +9,3 @@ def hello_world():
 
     return ("flask.render_template"
             "")
-
-@app.route('/search/', methods=['POST'])
-def search():
-    error = None
-    global count, meteo, s
-    s = ''
-    count += 1
-    qwery = request.get_data()
-    meteo = json.loads(qwery)
-    for key in meteo:
-        s =s + key + '  ' + str(meteo[key]) + '<br>'
-    print(s, count, '<br>' )
-    return ''
-if __name__ == "__main__":
-    meteo = ''
-    s ='no_data'
-    count = 0
-    app.run('0.0.0.0',port=server_port)
